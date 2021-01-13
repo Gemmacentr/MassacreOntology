5488e111efce7de933c4259f701df7c89641ad08
Gemma Centrone <56439561+Gemmacentr@users.noreply.github.com>
2021-01-13 11:27:17 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function lentep() {
+document.img1.width=document.img1.width*3
+document.img1.height=document.img1.height*3
+}
+function lentem() {
+document.img1.width=document.img1.width/3
+document.img1.height=document.img1.height/3
+}
